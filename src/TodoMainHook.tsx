bc2ab66905981f28572d90b44a22d430561075c6
“helpkang” <helpkang@gmail.com>
2024-03-12 14:08:26 +0900

    themehook init 작업 여부

--- src/TodoMainHook.tsx
+++ src/TodoMainHook.tsx
@@ -17,16 +17,9 @@ const TodoMainHook = () => {
     error,
     setError,
   } = useTodoService();
-  
-  const { theme } = useThemeService();
-  useEffect(() => {
-    console.log("theme change", theme);
-    const bgLight: string = "hsl(0, 0%, 98%)";
-    const bgDark: string = "hsl(235, 21%, 11%)";
-    theme === "light"
-      ? (document.body.style.backgroundColor = bgLight)
-      : (document.body.style.backgroundColor = bgDark);
-  }, [theme]);
+
+  const { theme } = useThemeService(true);
+
 
   return (
     <div className="container" data-theme={theme}>
--- src/service/useThemeService.ts
+++ src/service/useThemeService.ts
@@ -1,7 +1,24 @@
 import { useEffect } from "react";
-import { useThemeContextRepository } from "../repository/useThemeContextRepository";
+import {
+  ThemeType,
+  useThemeContextRepository,
+} from "../repository/useThemeContextRepository";
 
-export function useThemeService() {
+export function useThemeService(init = false) {
   const { theme, toggle } = useThemeContextRepository();
+  if (init) {
+    initEffect(theme);
+  }
   return { theme, toggle };
+}
+
+function initEffect(theme: ThemeType) {
+  useEffect(() => {
+    console.log("theme change", theme);
+    const bgLight: string = "hsl(0, 0%, 98%)";
+    const bgDark: string = "hsl(235, 21%, 11%)";
+    theme === "light"
+      ? (document.body.style.backgroundColor = bgLight)
+      : (document.body.style.backgroundColor = bgDark);
+  }, [theme]);
 }
